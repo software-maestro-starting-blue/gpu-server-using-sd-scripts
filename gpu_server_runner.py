d99483a81a91e7f7fd6a75a8959e5a10d3ca30ba
JuneBridge <junebridge03@g.skku.edu>
2024-10-16 16:56:56 +0900

    feat: stable diffusion 모델과 lora 모델을 preload하는 로직을 구성함.

--- gpu_server_runner.py
+++ gpu_server_runner.py
@@ -9,12 +9,70 @@ from sdxl_gen_img import setup_parser, setup_logging, main
 
 from sdxl_gen_img_preloader import preload, preload_lora
 
+
+'''
+이 코드에서 Stable Diffusion 모델과 LoRA 모델을 preload합니다.
+'''
+parser = setup_parser()
+args = parser.parse_args()
+
+args.ckpt = "./models/sd_xl_base_1.0_0.9vae.safetensors"
+args.xformers = True
+args.bf16 = True
+args.W = 512
+args.H = 512
+args.scale = 7.0
+args.sampler = 'dpmsolver++'
+args.steps = 70
+args.batch_size = 1
+args.images_per_prompt = 1
+
+setup_logging(args)
+
+
+args.network_module = ['networks.lora']
+lora_paths = {
+    2: "./lora/model_2.safetensors",
+}
+lora_muls = {
+    2: 1.0,
+}
+
+
+preload_model_by_character_id = {}
+preload_loras = {}
+
+for character_id, lora_path in lora_paths.items():
+    args.network_weights = [lora_path]
+    args.network_mul = [lora_muls[character_id]]
+
+    dtype, highres_fix, text_encoder1, text_encoder2, vae, unet, tokenizer1, tokenizer2, scheduler_num_noises_per_step, noise_manager, scheduler, device = preload(args)
+    
+    preload_model_by_character_id[character_id] = (dtype, highres_fix, text_encoder1, text_encoder2, vae, unet, tokenizer1, tokenizer2, scheduler_num_noises_per_step, noise_manager, scheduler, device)
+    preload_loras[character_id] = preload_lora(args, vae, text_encoder1, text_encoder2, unet, dtype, device)
+
+def generate_image_sdxl_with_lora(character_id, prompt, output_dir):
+    parser = setup_parser()
+    args = parser.parse_args()
+
+    args.outdir = output_dir
+    args.prompt = prompt
+
+    dtype, highres_fix, text_encoder1, text_encoder2, vae, unet, tokenizer1, tokenizer2, scheduler_num_noises_per_step, noise_manager, scheduler, device = preload_model_by_character_id[character_id]
+    networks, network_default_muls, network_pre_calc = preload_loras[character_id]
+
+    main(args, dtype, highres_fix, text_encoder1, text_encoder2, vae, unet, tokenizer1, tokenizer2, scheduler_num_noises_per_step, noise_manager, scheduler, device, networks, network_default_muls, network_pre_calc)
+
+
+'''
+이 코드에서는 메시지를 받고 이미지를 생성한 뒤 S3에 업로드합니다.
+'''
+
 def extract_message(message_body):
     message = json.loads(message_body)
     return message['diaryId'], message['characterId'], message['prompt'], message['gridPosition']
 
-def get_image_path(diary_id, grid_position):
-    folder_path = f"./output/{diary_id}_{grid_position}"
+def get_image_path(folder_path):
     png_files = glob.glob(os.path.join(folder_path, '*.png'))
     if not png_files:
         return None
@@ -23,34 +81,6 @@ def get_image_path(diary_id, grid_position):
 
 def delete_output_images():
     pass
-
-def generate_image_sdxl_with_lora(lora_model, prompt, output_dir):
-    parser = setup_parser()
-    args = parser.parse_args()
-
-    args.ckpt = "./models/sd_xl_base_1.0_0.9vae.safetensors"
-    args.outdir = output_dir
-    args.xformers = True
-    args.bf16 = True
-    args.W = 512
-    args.H = 512
-    args.scale = 7.0
-    args.sampler = 'dpmsolver++'
-    args.network_module = ['networks.lora']
-    args.network_weights = [lora_model]
-    args.network_mul = [1.0]
-    args.steps = 70
-    args.batch_size = 1
-    args.images_per_prompt = 1
-    args.prompt = prompt
-
-    setup_logging(args)
-
-    dtype, highres_fix, text_encoder1, text_encoder2, vae, unet, tokenizer1, tokenizer2, scheduler_num_noises_per_step, noise_manager, scheduler, device = preload(args)
-
-    networks, network_default_muls, network_pre_calc = preload_lora(args, vae, text_encoder1, text_encoder2, unet, dtype, device)
-
-    main(args, dtype, highres_fix, text_encoder1, text_encoder2, vae, unet, tokenizer1, tokenizer2, scheduler_num_noises_per_step, noise_manager, scheduler, device, networks, network_default_muls, network_pre_calc)
 
 def run():
     while True:
@@ -63,12 +93,11 @@ def run():
         for message_body, message_receipt_handle in messages:
             diary_id, character_id, prompt, grid_position = extract_message(message_body)
 
-            lora_model = f"./lora/model_{character_id}.safetensors"
             output_dir = f"./output/{diary_id}_{grid_position}"
 
             try:
-                generate_image_sdxl_with_lora(lora_model, prompt, output_dir)
-                image_path = get_image_path(diary_id, grid_position)
+                generate_image_sdxl_with_lora(character_id, prompt, output_dir)
+                image_path = get_image_path(output_dir)
 
                 upload_image_to_s3(image_path, diary_id, grid_position)
 
